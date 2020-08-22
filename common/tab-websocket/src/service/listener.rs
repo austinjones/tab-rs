c9fafd788e70068e90b6619851148df3ed1595e0
Austin Jones <austin.jones@threatx.com>
2020-08-22 19:13:56 -0400

    Update listener.rs

--- common/tab-websocket/src/service/listener.rs
+++ common/tab-websocket/src/service/listener.rs
@@ -10,7 +10,6 @@ use crate::{
     },
 };
 use log::{debug, error};
-use std::net::SocketAddr;
 use tab_service::{dyn_bus::DynBus, Bus, Lifeline, Service};
 use tokio::{net::TcpListener, sync::mpsc};
 pub struct WebsocketListenerService {
