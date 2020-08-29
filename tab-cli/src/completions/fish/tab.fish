fef17049e56994d447c24489fe98c2ac5eb28b23
Austin Jones <austinbaysjones@gmail.com>
2020-08-29 02:11:43 -0400

    Considerably improved autocomplete scripts (#83)
    Improve the autocomplete scripts for bash, fish, and zsh.
    
    Update README.md with an intro section, and a few other tweaks.

--- tab-cli/src/completions/fish/tab.fish
+++ tab-cli/src/completions/fish/tab.fish
@@ -1,9 +1,12 @@
 # `tab` autocompletions for fish
 
-complete -c tab -n "__fish_use_subcommand" -f -a '(tab --_autocomplete_tab)' 
-complete -c tab -n "__fish_use_subcommand" -s w -l close -d 'closes the tab with the given name' -x -a '(tab --_autocomplete_close_tab)'
+complete -c tab  -f -a '(tab --_autocomplete_tab)' 
+# hack here with `-o w`, to get fish to insert a space after the `tab -w` completion
+complete -c tab -n "__fish_use_subcommand" -o w -l close -d 'closes the tab with the given name' -x -a '(tab --_autocomplete_close_tab)'
 
+complete -c tab -l completion -d 'prints raw autocomplete scripts' -x -a 'bash elvish fish powershell zsh'
 complete -c tab -n "__fish_use_subcommand" -s l -l list -d 'lists the active tabs'
 complete -c tab -n "__fish_use_subcommand" -s W -l shutdown -d 'terminates the tab daemon and all active pty sessions'
 complete -c tab -n "__fish_use_subcommand" -s h -l help -d 'Prints help information'
 complete -c tab -n "__fish_use_subcommand" -s V -l version -d 'Prints version information'
+
