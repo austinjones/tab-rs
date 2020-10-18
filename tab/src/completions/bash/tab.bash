ad33dceb6acde88ffec4f8d1e71968143b0012f2
Austin Jones <implAustin@gmail.com>
2020-10-17 21:07:59 -0400

    Autocompletion fixes

--- tab/src/completions/bash/tab.bash
+++ tab/src/completions/bash/tab.bash
@@ -1,25 +1,32 @@
 # `tab` autocompletions for bash
 
-_tab_select() {
-    tab --_autocomplete_tab
-}
-
-_tab_close() {
-    tab --_autocomplete_close_tab
-}
-
 _tab() {
     local cur prev opts
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     case "$prev" in
-    @(-w|--close|-z|--disconnect) )
+    -w)
         TABS=$(tab --_autocomplete_close_tab)
         COMPREPLY=( $(compgen -W "${TABS}" -- $cur) )
         return 0
         ;;
-    --completion                  )
+    --close)
+        TABS=$(tab --_autocomplete_close_tab)
+        COMPREPLY=( $(compgen -W "${TABS}" -- $cur) )
+        return 0
+        ;;
+    -z)
+        TABS=$(tab --_autocomplete_close_tab)
+        COMPREPLY=( $(compgen -W "${TABS}" -- $cur) )
+        return 0
+        ;;
+    --disconnect)
+        TABS=$(tab --_autocomplete_close_tab)
+        COMPREPLY=( $(compgen -W "${TABS}" -- $cur) )
+        return 0
+        ;;
+    --completion)
         COMPREPLY=( $(compgen -W "bash elvish fish powershell zsh") )
         return 0
         ;;
@@ -27,12 +34,13 @@ _tab() {
 
     case "$cur" in
     --completion=)
+        TABS=$(tab --_autocomplete_close_tab)
         COMPREPLY=( $(compgen -W "bash elvish fish powershell zsh") )
         return 0
         ;;
     -*)
-        opts=" -h --help -l --list -w --close -W --shutdown -V --version --completion <TAB> "
-        COMPREPLY=( $(compgen -W "${opts}") )
+        opts=" -h --help -l --list -w --close -z --disconnect -W --shutdown -V --version --completion <TAB> "
+        COMPREPLY=( $(compgen -W "${opts}" -- $cur) )
         return 0
         ;;
     *)
