589cfb8851e2bf9a054e51b95087acf7e8a249da
Austin Jones <austinbaysjones@gmail.com>
2020-12-17 20:54:33 -0500

    Add a regression test that requires 5ms keyboard latency or better
    Also add log statements with await_stdout timing information

---
+++ tab/tests/keyboard_latency.rs
@@ -0,0 +1,24 @@
+mod common;
+use common::*;
+
+/// Tests that two sessions can be established (A and B),
+///  that A can be reconnected to,
+///  and B can be switched to by executing the tab binary within A.
+#[tokio::test]
+async fn keyboard_latency() -> anyhow::Result<()> {
+    let session = TestSession::new()?;
+
+    // check that keyboard latency is under 5ms
+    session
+        .command()
+        .strict_timeout()
+        .tab("latency-test/")
+        .await_stdout("$", 1000)
+        .stdin("!!")
+        .await_stdout("!!", 5)
+        .stdin_bytes(&[0x14, 0x1b])
+        .run()
+        .await?;
+
+    Ok(())
+}
