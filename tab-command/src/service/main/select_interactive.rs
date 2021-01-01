daacf87a7c8d93f29e6e07aa06869fd8b5ca7466
Austin Jones <austinbaysjones@gmail.com>
2021-01-01 15:33:06 -0500

    Prevent the fuzzy finder from being used within a session (#293)

--- tab-command/src/service/main/select_interactive.rs
+++ tab-command/src/service/main/select_interactive.rs
@@ -1,5 +1,8 @@
 use crate::{
-    message::main::MainRecv, message::tabs::TabRecv, message::terminal::TerminalRecv, prelude::*,
+    message::main::MainRecv,
+    message::tabs::TabRecv,
+    message::{main::MainShutdown, terminal::TerminalRecv},
+    prelude::*,
     state::terminal::TerminalMode,
 };
 
@@ -16,11 +19,25 @@ impl Service for MainSelectInteractiveService {
 
         let mut tx_terminal = bus.tx::<TerminalRecv>()?;
         let mut tx_tab = bus.tx::<TabRecv>()?;
+        let mut tx_shutdown = bus.tx::<MainShutdown>()?;
 
         let _run = Self::try_task("run", async move {
             while let Some(msg) = rx.recv().await {
                 if let MainRecv::SelectInteractive = msg {
                     info!("MainRecv::SelectInteractive running");
+
+                    // Prevent the fuzzy finder from being used within an active session.
+                    // See https://github.com/austinjones/tab-rs/issues/262
+                    // Soon this will be replaced with a retask of the outer command client.
+                    if std::env::var("TAB_ID").is_ok() {
+                        eprintln!(
+                            "You are within an active session.  Press ctrl-T to open the fuzzy finder."
+                        );
+
+                        tx_shutdown.send(MainShutdown(1)).await?;
+                        continue;
+                    }
+
                     tx_terminal
                         .send(TerminalRecv::Mode(TerminalMode::FuzzyFinder))
                         .await?;
