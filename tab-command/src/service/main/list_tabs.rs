austinjones/tab-rs

commit a9c269fc09beaea2419a1fd7b68003b8107a5d3c

Fix clippy warnings and errors (#366)
* Clippy fixes

* Update workspace.rs

* Update chunk.rs

* Update connection.rs

* Fix a bug in the clippy edit

* Fix a few findings that only appeared on CI

tab-command/src/service/main/list_tabs.rs
@@ -2,9 +2,9 @@ use crate::{
     message::main::MainRecv, message::main::MainShutdown, prelude::*,
     state::workspace::WorkspaceState, state::workspace::WorkspaceTab, utils::await_state,
 };
-use std::env;
 use std::io::stdout;
 use std::path::PathBuf;
+use std::{env, path::Path};
 
 use crossterm::{
     execute,
@@ -30,9 +30,9 @@ impl Service for MainListTabsService {
                 if let MainRecv::ListTabs = msg {
                     let workspace = await_state(&mut rx_workspace).await?;
 
-                    if workspace.errors.len() > 0 {
+                    if workspace.errors.is_empty() {
                         eprintln!("Workspace errors were found during startup.  Use `tab --check` for more details.");
-                        eprintln!("");
+                        eprintln!();
                     }
 
                     Self::echo_tabs(&workspace.tabs);
@@ -49,10 +49,10 @@ impl Service for MainListTabsService {
 }
 
 impl MainListTabsService {
-    fn echo_tabs(tabs: &Vec<WorkspaceTab>) {
+    fn echo_tabs(tabs: &[WorkspaceTab]) {
         debug!("echo tabs: {:?}", &tabs);
 
-        if tabs.len() == 0 {
+        if tabs.is_empty() {
             println!("No active tabs.");
             return;
         }
@@ -81,13 +81,13 @@ impl MainListTabsService {
                 }
                 println!("({})", doc);
             } else {
-                println!("");
+                println!();
             }
         }
     }
 }
 
-fn is_active(tab: &WorkspaceTab, cwd: &PathBuf) -> bool {
+fn is_active(tab: &WorkspaceTab, cwd: &Path) -> bool {
     cwd.starts_with(tab.directory.as_path())
 }
 

common/tab-api/src/chunk.rs
@@ -20,6 +20,10 @@ impl OutputChunk {
         self.data.len()
     }
 
+    pub fn is_empty(&self) -> bool {
+        self.data.is_empty()
+    }
+
     /// Returns true if this chunk's data contains the given index
     pub fn contains(&self, index: usize) -> bool {
         index >= self.start() && index < self.end()
@@ -72,9 +76,14 @@ pub struct InputChunk {
 }
 
 impl InputChunk {
-    /// THe data buffer length
+    /// The data buffer length
     pub fn len(&self) -> usize {
         self.data.len()
+    }
+
+    /// Whether the data is empty
+    pub fn is_empty(&self) -> bool {
+        self.data.is_empty()
     }
 }
 

common/tab-api/src/launch.rs
@@ -102,7 +102,7 @@ pub async fn wait_for_shutdown<T: Default>(mut receiver: impl Stream<Item = T> +
                 return T::default();
             },
             msg = receiver.recv() => {
-                return msg.unwrap_or(T::default());
+                return msg.unwrap_or_default();
             }
         }
     }

common/tab-api/src/log.rs
@@ -21,13 +21,9 @@ pub fn get_level() -> Option<LevelFilter> {
 }
 
 pub fn get_level_str() -> Option<&'static str> {
-    let level = get_level();
+    let level = get_level()?;
 
-    if let None = level {
-        return None;
-    }
-
-    match level.unwrap() {
+    match level {
         LevelFilter::Off => Some("off"),
         LevelFilter::Error => Some("error"),
         LevelFilter::Warn => Some("warn"),

common/tab-api/src/tab.rs
@@ -7,7 +7,7 @@ use std::{collections::HashMap, fmt::Display, num::ParseIntError, str::FromStr};
 
 pub fn normalize_name(name: &str) -> String {
     let name = name.to_string().trim().to_string();
-    if name.ends_with("/") {
+    if name.ends_with('/') {
         name
     } else {
         name + "/"

common/tab-websocket/src/auth.rs
@@ -65,7 +65,7 @@ impl AuthHandler {
 
         let expected_token = self.token.0.as_ref().unwrap().as_str();
 
-        if !request.headers().get("authorization").is_some() {
+        if request.headers().get("authorization").is_none() {
             return AuthState::RejectAuth;
         }
 
@@ -94,7 +94,7 @@ impl Callback for AuthHandler {
         if let Some(send_metadata) = self.send_metadata {
             let uri = request.uri().clone();
             let method = request.method().clone();
-            let metadata = RequestMetadata { uri, method };
+            let metadata = RequestMetadata { method, uri };
             tokio::spawn(send_metadata.reply(|_r| async { metadata }));
         }
 

common/tab-websocket/src/common.rs
@@ -19,9 +19,5 @@ pub async fn send_close(websocket: &mut WebsocketConnection) {
 
 /// Checks if, given the message, the connection should be closed.
 pub fn should_terminate(message: &Message) -> bool {
-    if let Message::Close(_) = message {
-        true
-    } else {
-        false
-    }
+    matches!(message, Message::Close(_))
 }

common/tab-websocket/src/service/connection.rs
@@ -64,11 +64,11 @@ async fn runloop(
     mut tx: impl Sink<Item = WebsocketRecv> + Unpin,
 ) -> anyhow::Result<()> {
     let websocket = &mut websocket_drop.0;
-    info!("starting runloop");
+    debug!("starting runloop");
     loop {
         select!(
             message = websocket.next() => {
-                if let None = message {
+                if message.is_none() {
                     info!("terminating - websocket disconnected");
                     break;
                 }

commit 44554f83fec04d79f000d7058fd374cef677d0d7

Fix incorrect message about workspace errors
There appears to have been a minor bug introduced in #366 which meant
that when a user listed tabs using the CLI, a message was diplayed
warning the user of errors, when no errors were present.

I've added a one liner change, just swapped out an is_empty() with an
!is_empty(). This was all that was needed to resolve the issue.

Signed-off-by: Josh Bainbridge <josh.bainbridge@gmail.com>

tab-command/src/service/main/list_tabs.rs
@@ -30,7 +30,7 @@ impl Service for MainListTabsService {
                 if let MainRecv::ListTabs = msg {
                     let workspace = await_state(&mut rx_workspace).await?;
 
-                    if workspace.errors.is_empty() {
+                    if !workspace.errors.is_empty() {
                         eprintln!("Workspace errors were found during startup.  Use `tab --check` for more details.");
                         eprintln!();
                     }